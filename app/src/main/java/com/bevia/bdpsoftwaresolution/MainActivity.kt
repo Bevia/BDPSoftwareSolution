51ba8a758b391307c1d515a6b8b95ef695fd761e
vbevia <vbevia@gmail.com>
2024-04-23 13:34:57 +0200

    add intent validation to onCreate

--- app/src/main/java/com/bevia/bdpsoftwaresolution/MainActivity.kt
+++ app/src/main/java/com/bevia/bdpsoftwaresolution/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +22,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        resultTextView = findViewById(R.id.resultTextView)
+
+        // Get the intent that started this activity
+        // in case a onNewIntent() is not called
+        val intent = intent
+        Log.d("DEBUG_INTENT", "intent $intent")
+        // Check if the intent is null
+        //retrieve intent extra data including message.
+        if (intent != null && intent.hasExtra("status")) {
+            val status = intent.getIntExtra("status", 0)
+            val message = intent.getStringExtra("message") ?: ""
+
+            resultTextView.text = "Result intent via onCreate"
+
+            //call the payment App here
+
+        }else{
+            //TODO: run your default code here
+            //Encapsulate your default code in a private method and call it here
+
+            resultTextView.text = "No intent data found"
+
+            Log.d("DEBUG_INTENT", "run your default code here")
         }
 
         Log.d("DEBUG_INTENT", "onCreate")
@@ -37,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         Log.d("DEBUG_INTENT", "onNewIntent")
+        resultTextView.text = "Result intent via onNewIntent"
 
         //call the payment App here
     }
